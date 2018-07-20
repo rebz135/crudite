7d3af4f8e36110ddde7b0c00079182971e79ded3
JohnMichelin <jmichelinphone@gmail.com>
2018-07-20 11:31:28 -0700

    feat(localStorage): add hardcoded event tied local storage interaction

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  // attach event listener to buttons(input?)
+  // create function stub for read/write/delete
+    // research local storage
+
+  // $().on('click', function(){
+  //
+  // });
+
+  $('.store-btn').on('click', function(event){
+    localStorage.setItem('hrext', "three is the best");
+  });
+
+  $('.get-btn').on('click', function(event){
+    console.log(localStorage.getItem('hrext'));
+  });
+
+  $('.delete-btn').on('click', function(event){
+    console.log(localStorage.removeItem('hrext'));
+  });
+
+
+});
